798824a096b3a443e9c8587025b0b93d60b352d3
MuhammedSercan <msercanustfdm@gmail.com>
2023-03-09 10:35:59 +0300

    last2

--- src/test/java/pages/DataTablesPage.java
+++ src/test/java/pages/DataTablesPage.java
@@ -24,6 +24,9 @@ public class DataTablesPage {
     @FindBy(xpath = "//*[@id='DTE_Field_office']")
     public WebElement office;
 
+    @FindBy(xpath = "//*[@id='DTE_Field_office']")
+    public WebElement office2;
+
     @FindBy(xpath = "//*[@id='DTE_Field_extn']")
     public WebElement extension;
 
